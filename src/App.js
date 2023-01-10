581105bb75ef5481edd4c32591ac036504fa1773
Amida <amidamri12@gmail.com>
2023-01-10 19:53:48 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav/Nav";
+import Products from "./components/Products/Products";
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading/Loading";
+import { CartProvider } from "./contex/CartContext";
+import ShoppingCart from "./components/Cart/ShoppingCart";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState("");
+  const [sort, setSort] = useState("Featured");
+  const [filter, setFilter] = useState("all products");
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const url = "https://fakestoreapi.com/products";
+      const data = (await fetch(url)).json();
+      setItems(await data);
+      console.log("Getting Data");
+    };
+    fetchProducts().then(() => setIsLoading(false));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="App">
+        <Loading />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CartProvider>
+        <Nav setFilter={setFilter} passItems={items} />
+        <Products passFilter={filter} passItems={items} />
+      </CartProvider>
     </div>
   );
 }
