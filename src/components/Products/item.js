af2a005e7c9d5823ce9cc0a8af8cbd177c2792dd
Ami <amidamri12@gmail.com>
2023-01-14 15:18:40 +0200

    added routes

--- src/components/Products/item.js
+++ src/components/Products/item.js
@@ -24,16 +24,17 @@ const Item = (props) => {
                 <h6>${price}</h6>
                 <div className="buttons">
                     {quantity === 0 ? (
-                        <button onClick={() => cartContext.addItem(props.item)} className="add-to-cart">Add to
-                            Cart</button>) : null
+                        <button  onClick={() => cartContext.addItem(props.item)} className="add-to-cart">Add to
+                            Cart</button>) : <>
+                        <button className="button-increment"
+                                onClick={() => cartContext.incrementProduct(props.item.id)}>+
+                        </button>
+                        <p>In cart: {quantity}</p>
+                        <button className="button-decrement"
+                                onClick={() => cartContext.decrementProduct(props.item.id)}>-
+                        </button>
+                    </>
                     }
-                    {quantity >= 1 ? (
-                        <div>
-                            <button className="button-increment" onClick={() => cartContext.incrementProduct(props.item.id)}>+</button>
-                            <p>In cart: {quantity}</p>
-                            <button className="button-decrement" onClick={() => cartContext.decrementProduct(props.item.id)}>-</button>
-                        </div>
-                    ) : null}
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Main from "./Main";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
